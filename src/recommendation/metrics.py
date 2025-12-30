ef241ec3d4fd7897dc644e2ddfa6ee386d318410
VishuPatel <vishupatel575@gmail.com>
2025-12-30 17:45:19 -0500

    fix: test ruff locally

--- src/recommendation/metrics.py
+++ src/recommendation/metrics.py
@@ -3,11 +3,13 @@
 # Copyright The OpenTelemetry Authors
 # SPDX-License-Identifier: Apache-2.0
 
-def init_metrics(meter):
 
+def init_metrics(meter):
     # Recommendations counter
     app_recommendations_counter = meter.create_counter(
-        'app_recommendations_counter', unit='recommendations', description="Counts the total number of given recommendations"
+        "app_recommendations_counter",
+        unit="recommendations",
+        description="Counts the total number of given recommendations",
     )
 
     rec_svc_metrics = {
